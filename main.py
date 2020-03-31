2317dbe1d7b58c35f219ac985b420c9821c68da3
Jalmari Ikävalko <tzaeru@gmail.com>
2020-03-31 10:47:46 +0300

    Dataa

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 sns.set(style="ticks")
 
 # Load the example dataset for Anscombe's quartet
-df = pd.read_csv('heart.csv')
+df = pd.read_csv('cardio_train.csv', sep=';', nrows=100)
+
+
 
 # Show the results of a linear regression within each dataset
-sns.lmplot(x="age", y="target", data=df,
-           ci=None, palette="muted",
+sns.lmplot(x="ap_hi", y="cardio", data=df,
+           ci=None, palette="muted", hue="gluc",
            scatter_kws={"s": 50, "alpha": 1})
 
-#sns.relplot(x="trestbps", y="age", hue="target", style="target",
+#sns.lineplot(x="age", y="ap_hi",
+#             data=df)
+
+#sns.relplot(x="trestbps", y="age", hue="num", style="num",
 #            data=df)
 
+#plt.plot([1, 2, 3, 4])
+#plt.ylabel('some numbers')
 plt.show()
